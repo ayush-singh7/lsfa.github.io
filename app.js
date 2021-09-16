0725d2216e95cbc804f739c7a6422a74a8e0ebc3
ayush-singh7 <80507130+ayush-singh7@users.noreply.github.com>
2021-09-16 12:57:00 +0530

    chewy suarez

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 800;
+
+//Luis suarez is my favourite footballer unironically
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
+app.use(express.urlencoded())
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+
+// ENDPOINTS
+app.get('/', (req, res)=>{
+
+    const params = { }
+    res.status(200).render('index.pug', params);
+});
+
+app.get('/contact', (req, res)=>{
+
+    const params = { }
+    res.status(200).render('contact.pug', params);
+});
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`The application started sssssuccessfully on port ${port}`);
+});
